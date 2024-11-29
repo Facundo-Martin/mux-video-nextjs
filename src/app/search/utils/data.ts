0bf82d151b1855ab53b65ef1262c3090add58f9a
Facundo Martin <facundomartin98@gmail.com>
2024-11-29 18:10:41 -0300

    chore: hardcode mux video sample data

---
+++ src/app/search/utils/data.ts
@@ -0,0 +1,92 @@
+export const MUX_VIDEOS = [
+  {
+    teacher: "Molly Miller",
+    classTitle: "String Theory Part 1 - Triads",
+    playbackId: "PtLet5EWczosEXd5iP4ohjks02hCif2YUrH8CM6LPM9c",
+  },
+  {
+    teacher: "Joshua Starkman",
+    classTitle: "Rhythm Guitar for Accompaniment",
+    playbackId: "XYbWh2R0201mOdfOJVZdE9ebnyvj3eclmUAw9i79SsHL00",
+  },
+  {
+    teacher: "Brandon Paul",
+    classTitle: "Speed and Alternate Picking",
+    playbackId: "x6tuIJT5gWhqB7jeoGc5h4haLxsaOECTe7wyY6s1b84",
+  },
+  {
+    teacher: "Ruben Wan",
+    classTitle: "Best Practices for Learning Guitar ",
+    playbackId: "5xI8KnEA8IxAHN7leGVZ9XV02XjhHAYu01M01L4kH4KVuU",
+  },
+  {
+    teacher: "Quist",
+    classTitle: "Blues Guitar Improvisation",
+    playbackId: "xMYwFrFH1GBu3XuD3Vsv00VyDu22gqD01V3VZcQtuW01Pw",
+  },
+  {
+    teacher: "Joey Weidner",
+    classTitle: "Get in the Groove!",
+    playbackId: "02b5srnkvNTLdUBzds01IMHF8jOgHg5FgItY29UzIVSaM",
+  },
+  {
+    teacher: "Seth Rosenbloom",
+    classTitle: "Introduction to Blues Guitar",
+    playbackId: "WUvUZnMslCJr7lFsm6OyJQcyQmMxc2MsMeOpRw01HQK8",
+  },
+  {
+    teacher: "Nena Shelby",
+    classTitle: "4 Chords: The Best of Taylor Swift",
+    playbackId: "B8g9FF232Upl425bT1Oy6UePOoRmn6eq5DNzbNHehG8",
+  },
+  {
+    teacher: "Molly Miller",
+    classTitle: "String Theory 2 - Melody Making ",
+    playbackId: "XGaNZ99gJj02irnUof02lj7007unl02z138z01bXkSGVtlsQ",
+  },
+  {
+    teacher: "Joshua Starkman",
+    classTitle: "Learn to Play Guitar By Ear",
+    playbackId: "QmsAI1vVBkpm9uRV018w0128c01uRnX5DqlVNSuil39P8o",
+  },
+  {
+    teacher: "Jude Smith",
+    classTitle: "Meaningful Chords, Meaningful Soloing",
+    playbackId: "zbyeTEOkvBfI02jcrudk5LzBPOJHZbZ9g67bxnSOeufc",
+  },
+  {
+    teacher: "Jake Fine",
+    classTitle: "Chord Vocabulary for Guitarists (Level 1)",
+    playbackId: "HRmSzFUZ3y7pUMEFNBKS4cb38GIuXwVQXE02Ov6rqXno",
+  },
+  {
+    teacher: "Molly Miller",
+    classTitle: "Chord Melody Part 1",
+    playbackId: "fuHYfcZ2qFfC19IfOU9KzfeX2WfSP6iZIJHciwMhj2M",
+  },
+  {
+    teacher: "Kaspar Jalily",
+    classTitle: "Mastering the Fretboard on Guitar",
+    playbackId: "yZk5XrHfhaug3p5XZ6ABX1NAj2yapGVOFejDPdEPRSA",
+  },
+  {
+    teacher: "Cecil Alexander",
+    classTitle: "Developing Jazz Vocabulary for Guitarists",
+    playbackId: "00KiTiWLiwxuplybY02J5LiPltuOEKDU8RBuOVVoSO1lw",
+  },
+  {
+    teacher: "Lyle Moxsom",
+    classTitle: "Learning Guitar The Intuitive Way",
+    playbackId: "M02z4LPqXghKT4001hL02Fu4kujim1p02onnxg4fCrGzouI",
+  },
+  {
+    teacher: "Federico Sanzini",
+    classTitle: "Blues & Rock Guitar Improvisation",
+    playbackId: "HkE01YBK45d02YF00lncFiGKFug02JdqbwqpGPpFDYwOr00M",
+  },
+  {
+    teacher: "Joey Weidner",
+    classTitle: "Jimi Hendrix: Axis Bold As Love",
+    playbackId: "LpY3RS01F7juhzE4h3AZcRT1A21C65ro01fVAd9hMEDRI",
+  },
+] as const;
